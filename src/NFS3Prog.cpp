743d8d2c732fe4fe9ffc7851e55117074eebacc3
fangjun.zhang <fangjun.zhang@qingteng.cn>
2026-02-26 16:32:03 +0800

    Implement SetActualLength method in opaque class and update ProcedureREAD to use it; update project configuration to use v142 toolset and add .vs directory to .gitignore

--- src/NFS3Prog.cpp
+++ src/NFS3Prog.cpp
@@ -147,6 +147,11 @@ void opaque::SetSize(uint32 len)
     memset(contents, 0, length);
 }
 
+void opaque::SetActualLength(uint32 len)
+{
+    length = len;
+}
+
 nfs_fh3::nfs_fh3() : opaque(NFS3_FHSIZE)
 {
 }
@@ -691,7 +696,7 @@ nfsstat3 CNFS3Prog::ProcedureREAD(void)
         if (pFile != NULL) {
             _fseeki64(pFile, offset, SEEK_SET);
             count = fread(data.contents, sizeof(char), count, pFile);
-            data.SetSize(count);
+            data.SetActualLength(count);
             eof = fgetc(pFile) == EOF;
             fclose(pFile);
         } else {
